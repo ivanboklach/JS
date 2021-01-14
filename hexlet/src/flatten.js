b7187adb30da996158e6ecee8784984a7f11d030
ivanboklach <iboklach@alfabank.ru>
2021-01-14 10:52:43 +0300

    added flatten

---
+++ hexlet/src/flatten.js
@@ -0,0 +1,9 @@
+import { l, isEmpty, isList, toString as listToString, head, tail, cons } from '../sequences/pairs-data';
+import { reverse, reduce } from '../sequences/lists';
+
+export default (list) => {
+  if (isEmpty(list)) return l();
+  const func = (x, acc) => !isList(x) ? cons(x, acc) : reduce(func, acc, x);
+  const result = reduce(func, l(), list);
+  return reverse(result);
+};
